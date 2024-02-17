fe7fcdcb5d6debafe724df6e06e1f7556f62dcfc
JR-Koders <jr.koders@gmail.com>
2024-02-17 14:13:02 -0500

    Made automatic color theme change
    When the user changes his machine's theme, it automatically changes the website's theme.
    Also when page load, automatic theme taken from machine

--- js/color-theme.js
+++ js/color-theme.js
@@ -1,16 +1,53 @@
+
+function dark() {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+}
+
+function light() {
+    document.documentElement.setAttribute('data-bs-theme', 'light')
+}
+
 document.getElementById('dark-mode-toggle-left').addEventListener('click', () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme', 'light')
+        light();
     }
     else {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        dark();
     }
 });
 document.getElementById('dark-mode-toggle-right').addEventListener('click', () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme', 'light')
+        light();
     }
     else {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        dark();
     }
 });
+
+
+// Check to see if Media-Queries are supported
+if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // Dark
+        dark();
+        console.log('using dark mode! switching to dark!');
+    } else {
+        // Light
+        light();
+        console.log('using light mode! switching to light!');
+    }
+} else {
+    // Default (when Media-Queries are not supported)
+    console.log('your browser doesn\'t support media queries...');
+}
+
+
+// if the user changes the machine's theme
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    console.log('changed color theme, new theme:', newColorScheme);
+    newColorScheme === "dark" ? dark() : light();
+});
+
+
